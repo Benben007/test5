e0a0f17777cc8c90aa8a6fddbbf9c8b2d6acec51
Benben007 <bernardsodbjork@gmail.com>
2017-03-30 17:44:04 +0200

    Update test5.js

--- test5.js
+++ test5.js
@@ -1,4 +1,4 @@
-//läxhjlp
+//budget
 var mysql = require('mysql');
 var express = require('express');
 var mustacheExpress = require('mustache-express');
@@ -32,6 +32,25 @@ function getData1(connection, keywork, callback){
 	connection.query(query1, callback);
 }
 
+function getData5(connection, keywork, callback){
+	query1='delete from Client where Client.PrenomClient like "' + prenomclient2 + '" and Client.NomClient like "' + nomclient2 + '" and Client.PrenomEleve like "' + prenomeleve2 + '" and Client.NomEleve like "' + nomeleve2 + '" and Client.Numeropersonel=' + numeropersonel2 + ' and Client.Telephone=' + telephone2 + ' and Client.Email="' + email2 + '"  and Client.Adresse="' + adresse2 + '" and Client.Matiere="' + matiere2 + '" '
+	console.log(query1)
+	connection.query(query1, callback);
+}
+
+
+function getData7(connection, keywork, callback){
+	query1='insert into Client(PrenomClient,NomClient,PrenomEleve,NomEleve,Numeropersonel,Telephone,Email,Adresse,Matiere) values ("' + prenomclient2 + '", "' + nomclient2 + '", "' + prenomeleve2 + '", "' + nomeleve2 + '", ' + numeropersonel2 + ', ' + telephone2 + ', "' + email2 + '", "' + adresse2 + '", "' + matiere2 + '")'
+	console.log(query1)
+	connection.query(query1, callback);
+}
+
+
+function getData6(connection, keywork, callback){
+	query1='SELECT PrenomClient as PrenomClient, NomClient as NomClient, PrenomEleve as PrenomEleve , NomEleve as NomEleve, Numeropersonel as Numeropersonel, Telephone as Telephone, Email as Email, Adresse as Adresse, Matiere as Matiere FROM Client'
+	console.log(query1)
+	connection.query(query1, callback);
+}
 
 var app = express();
 app.engine('html', mustacheExpress());
@@ -52,15 +71,26 @@ app.get('/', function (req, res) {
 	numerocompte1 = req.query['numerocompte1']
 	dateembauche1 = req.query['dateembauche1']
 	datefin1 = req.query['datefin1']
+
+	prenomclient2 = req.query['prenomclient2']
+	nomclient2 = req.query['nomclient2']
+	prenomeleve2 = req.query['prenomeleve2']
+	nomeleve2 = req.query['nomeleve2']
+	numeropersonel2 = req.query['numeropersonel2']
+	telephone2 = req.query['telephone2']
+	email2 = req.query['email2']
+	adresse2 = req.query['adresse2']
+	matiere2 = req.query['matiere2']
+
 	
 
 	var fonctionRetour4 = function(err, results, fields){
-		  getData1(connection, keywork, fonctionRetour1);	
+		  getData6(connection, keywork, fonctionRetour6);	
 
 	};
 
 	var fonctionRetour3 = function(err, results, fields){
-		  getData1(connection, keywork, fonctionRetour1);	
+		  getData6(connection, keywork, fonctionRetour6);	
 
 	};
 	
@@ -73,17 +103,20 @@ app.get('/', function (req, res) {
 
 //	 		console.log(results)
 				
+	 			
 	 			var data1 = []
 	 			for (var i in results) {
 	 			data1.push([results[i].Prenom, results[i].Nom, results[i].Numeropersonel, results[i].Telephone, results[i].Email, results[i].Adresse, results[i].Matiere, results[i].Numerocompte, results[i].Dateembauche, results[i].Datefin])
-
+	 			
 	 	  		}
 				dataAsString1 = JSON.stringify(data1)
+				
 
 //				console.log(dataAsString1);
 				
 				res.render('test5.html', {
 						data1: dataAsString1,
+						data2: dataAsString2,
 								
 				});
 				connection.end();
@@ -91,23 +124,82 @@ app.get('/', function (req, res) {
 			};
 	
 	};
-		
-		
 	
- if (req.query['del']){
- 	getData4(connection, keywork, fonctionRetour4);		
+	var fonctionRetour5 = function(err, results, fields){
+		  getData6(connection, keywork, fonctionRetour6);	
+
+	};
+
+	var fonctionRetour7 = function(err, results, fields){
+		  getData6(connection, keywork, fonctionRetour6);	
+
+	};
+	
+	var fonctionRetour6 = function(err, results, fields){
+		  if(err){
+		  	console.log(err);
+		  }
+		 	
+		 else {
+
+//	 		console.log(results)
+				
+	 			var data2 = []
+	 			
+	 			for (var i in results) {
+	 			data2.push([results[i].PrenomClient, results[i].NomClient, results[i].PrenomEleve, results[i].NomEleve, results[i].Numeropersonel, results[i].Telephone, results[i].Email, results[i].Adresse, results[i].Matiere])
+	 			
+	 	  		}
+				dataAsString2 = JSON.stringify(data2)
+
+				console.log(dataAsString2);
+				
+				
+				getData1(connection, keywork, fonctionRetour1);
+		
+			};
+	
+	};	
+
+ 	
+ if  ((req.query['del'])||(req.query['initiate'])) {
+	 if (req.query['del']){
+	 	getData4(connection, keywork, fonctionRetour4);		
+	 }
+	 else {
+	 	if (req.query['initiate']){
+				getData3(connection, keywork, fonctionRetour3);		
+		}
+		else {
+
+			getData6(connection, keywork, fonctionRetour6);	
+			
+		}
+		
+	 }
  }
  else {
- 	if (req.query['initiate']){
-			getData3(connection, keywork, fonctionRetour3);		
-	}
-	else {
+ 	if  ((req.query['del2'])||(req.query['initiate2'])) {
+	 if (req.query['del2']){
+	 	getData5(connection, keywork, fonctionRetour5);		
+	 }
+	 else {
+	 	if (req.query['initiate2']){
+				getData7(connection, keywork, fonctionRetour7);		
+		}
+		else {
 
-		getData1(connection, keywork, fonctionRetour1);	
+			getData6(connection, keywork, fonctionRetour6);	
+			
+		}
 		
-	}
-	
+	 }
+ 	}
+ 	else getData6(connection, keywork, fonctionRetour6);		
  }
+
+
+
 
 
 
@@ -132,11 +224,11 @@ app.listen( app.get( 'port' ), function() {
 // arbetare data 4 del, data1 show, data3 add 
 // kund  data5 del, data6 show, data7 add
 // mission data8 del, data9 show, data10 add 
-// si del->4->1      sinon si initiate->3->1     sinon 1  exit 
-// si del2->5->6 	sinon si initiate2->7->6     sinon 6 exit
+// si del:4->6->1      sinon si initiate->3->6->1     sinon 6->1  exit 
+// si del2:5->6->1 	sinon si initiate2:7->6->1     sinon 6->1  exit
 // si del3->8->9     sinon si initiate3->10->9     sinon 9 exit
 
-/*if (req.query['del'] || req.query['initiate'])
+/*if (req.query['del'] || req.query['initiate']){
 
 	 if (req.query['del']){
 	 	getData4(connection, keywork, fonctionRetour4);		
@@ -151,4 +243,21 @@ app.listen( app.get( 'port' ), function() {
 			
 		}
 		
-	 }*/
+	 }
+}
+else {
+	 if (req.query['del2']){
+	 	getData5(connection, keywork, fonctionRetour5);		
+	 }
+	 else {
+	 	if (req.query['initiate2']){
+				getData7(connection, keywork, fonctionRetour7);		
+		}
+		else {
+
+			getData6(connection, keywork, fonctionRetour6);	
+			
+		}
+		
+	 }	
+}*/
